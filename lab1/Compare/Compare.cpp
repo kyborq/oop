c6e55c22f49bf72cb7f1018e9eb2884bad773db3
Konstantin <52314985+kyborq@users.noreply.github.com>
2022-06-18 18:30:56 +0300

    Refactored function and driver programm

--- lab1/Compare/Compare.cpp
+++ lab1/Compare/Compare.cpp
@@ -9,26 +9,21 @@ int compare_files(string source_file_name, string compare_file_name);
 
 int main(int argc, char* argv[])
 {
-	string source_file_name;
-	string compare_file_name;
-	int compare_result;
-
 	setlocale(0, "");
 
 	if (argc <= 2)
 	{
-		cout << "Использование compare.exe file1.txt file2.txt" << endl;
+		cerr << "Ожидалось аргументов 2, передано " << argc - 1 << endl << "Использование compare.exe file1.txt file2.txt" << endl;
 		return 2;
 	}
 
-	source_file_name = argv[1];
-	compare_file_name = argv[2];
-
-	compare_result = compare_files(source_file_name, compare_file_name);
+	string source_file_name = argv[1];
+	string compare_file_name = argv[2];
+	int compare_result = compare_files(source_file_name, compare_file_name);
 
 	if (compare_result == -1)
 	{
-		cout << "Не удалось открыть файлы. Проверьте существование " << source_file_name << " и " << compare_file_name << endl;
+		cerr << "Не удалось открыть файлы. Проверьте существование " << source_file_name << " и " << compare_file_name << endl;
 		return 3;
 	}
 
@@ -51,20 +46,19 @@ int compare_files(string source_file_name, string compare_file_name)
 {
 	ifstream source_file(source_file_name);
 	ifstream compare_file(compare_file_name);
-	string source_line;
-	string compare_line;
-	int line_index = -1;
-	bool is_equal = false;
 
 	if (!source_file.is_open() || !compare_file.is_open())
 		return -1;
 
+	string source_line;
+	string compare_line;
+	int line_index = -1;
+
 	while (getline(source_file, source_line) && getline(compare_file, compare_line))
 	{
-		is_equal = compare_line == source_line;
 		line_index++;
 
-		if (!is_equal)
+		if (compare_line != source_line)
 			return line_index;
 	}
 }
